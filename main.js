28d95744b16c6c38aea0c9d125e5f7f77ad60287
Tamunoibi Aprekuma <taprekuma@yahoo.co.uk>
2019-03-21 13:19:19 +0100

    home page

---
+++ main.js
@@ -0,0 +1,39 @@
+/* 
+==================================> Ripple Effect on buttons
+ */
+const rippler = document.querySelectorAll('.rippler');
+
+Array.prototype.forEach.call(rippler, function(b){
+  b.addEventListener('click', createRipple);
+})
+
+function createRipple(e)
+{
+  if(this.getElementsByClassName('ripple').length > 0)
+    {
+      this.removeChild(this.childNodes[1]);
+    }
+  
+  let circle = document.createElement('div');
+  this.appendChild(circle);
+  
+  let d = Math.max(this.clientWidth, this.clientHeight);
+  circle.style.width = circle.style.height = d + 'px';
+  
+  circle.style.left = e.clientX - this.offsetLeft - d / 2 + 'px';
+  circle.style.top = e.clientY - this.offsetTop - d / 2 + 'px';
+  
+  circle.classList.add('ripple');
+}
+/* 
+==================================> Responsive top nav
+*/
+
+function ToggleDisplayNav() {
+  var x = document.getElementById("mynavBar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
